39513bbf74b0e66902df316713d531b7d9b4b4e0
jiushixiaoyang <qj21146@bristol.ac.uk>
2021-10-20 09:19:22 +0100

    some changes

--- Code/Week 3/R week3 practice.R
+++ Code/Week 3/R week3 practice.R
@@ -117,4 +117,87 @@ head(wad_dat)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 ##then read in the data using vroom:
-wad_dat <- vroom('./Bioinformatics_data-master/Workshop 3')
+my_data <- vroom('../Workshop 3/time_series_covid19_deaths_global.csv')
+
+##use vroom to read in some data from github:
+covid_dat <- vroom("https://raw.githubusercontent.com/jiushixiaoyang/bioinformatics/main/Code/Workshop%203/time_series_covid19_deaths_global.csv")
+
+##you can ignore this code for the moment if you want
+##but to briefly summarise it is reading in some data included in base R
+##and then splitting it into 3 differnt data.frame style objects based on the values in one of the columns ("cyl")
+mt <- tibble::rownames_to_column(mtcars, "model")
+purrr::iwalk(
+  split(mt, mt$cyl),
+  ##save this split files in to the default directory
+  ~ vroom_write(.x, glue::glue("mtcars_{.y}.csv"), "\t")
+)
+##find files in the default directory which start with "mtcars" and end in "csv"
+##save these file names as an object called "files"
+files <- fs::dir_ls(glob = "mtcars*csv")
+##these are then the names of the files matching the above arguments:
+files
+##then load these file names using vroom 
+vroom(files)
+
+##load in some RData(a file type)
+load("my_data/pathway/my_data.RData")
+
+vroom::vroom_write()
+library(vroom_write)
+##write out a .csv file
+vroom_write(my_data,"C:/Users/qj21146/Desktop/the_name_of_my_data.csv")
+?vroom_write()
+###### why can't find function vroom_write?????
+### finally it works!!!
+
+##write out my data as an RData file:
+save(my_data, file = "C:/Users/qj21146/Desktop/the_name_of_my_RData.RData")
+######not work#####work now!!!
+##write out my data as an RData file:
+save(my_data, 
+     my_vector, 
+     my_list, 
+     my_array,
+     file = "a pathway/a data folder/the_name_of_my_RData.RData")
+
+
+##install the tidyverse
+install.packages("tidyverse")
+##load the tidyverse
+library("tidyverse")
+##### can't see what guidance said in Global Environment
+
+
+##what class is the object
+class(covid_dat)
+##look at the data
+covid_dat
+####### the data is different from that in the guidance!!!
+
+##change the first two names of our data frame
+names(covid_dat)[1:2] <- c("Province.State", "Country.Region")
+covid_dat
+
+
+##so this says take our data frame called covid_dat
+covid_long <- covid_dat %>%
+  ##and then apply this function 
+  pivot_longer(cols = -c( Province.State, 
+                          Country.Region, 
+                          Lat, 
+                          Long))
+covid_long
+
+##our data frame
+covid_long <- covid_dat %>%
+  ##and then apply this function 
+  pivot_longer(cols = -c(Province.State:Long),
+               names_to = "Date",
+               values_to = "Deaths")
+covid_long
+
+##change long to wide
+covid_long %>% 
+  pivot_wider(names_from = Date,
+              values_from = Deaths)
+covid_lon#### why it's wide, I think it's still long???????
